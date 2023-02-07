fb85ea6e99b3235ca15421684f3e67ea1a93add6
PieMenard <pinedo.m.gustavo@gmail.com>
2023-02-07 12:11:15 -0400

    Complete rock_paper_scissors first working draft

--- javascript.js
+++ javascript.js
@@ -1,9 +1,25 @@
 let pcScore = 0;
 let playerScore = 0;
+const winScore = 5;
+
+const resultsBox = document.querySelector('.results');
+const plays = document.createElement('p');
+const score = document.createElement('p');
+const roundDetails = document.createElement('p');
+const winner = document.createElement('h1');
+const restart = document.createElement('button');
+
+resultsBox.appendChild(plays);
+resultsBox.appendChild(roundDetails);
+resultsBox.appendChild(score);
+resultsBox.appendChild(winner);
+
+const buttons = Array.from(document.querySelectorAll('button'));
+
 
 //generate computer choice
-function getComputerChoice()
-{
+function getComputerChoice(){
+
     let pcChoice = Math.floor(Math.random() * 3) + 1;
     switch (pcChoice)
     {
@@ -21,77 +37,95 @@ function getComputerChoice()
 
 //compare both plays and determine winner or tie
 
-function playRound(playerSelection)
-{
+function playRound(playerSelection){
+
     const computerSelection = getComputerChoice();
-    console.log("The computer chose "+computerSelection);
-    console.log("You chose "+playerSelection);
+    plays.textContent = "The computer chose "+computerSelection + '\n' + ", you chose "+playerSelection;
+    
+    updateScore();
+
     if (playerSelection === computerSelection)
     {
-            return "It's a tie";
+        roundDetails.textContent = "It's a tie"
     }
     if (playerSelection == "rock" && computerSelection == "paper")
     {
         pcScore += 1;
-        return "You lose, paper beats rock";
+        roundDetails.textContent =  "You lose, paper beats rock";
     }
     if (playerSelection == "rock" && computerSelection == "scissors")
     {
         playerScore += 1;
-        return "You win, rock beats scissors";
+        roundDetails.textContent =  "You win, rock beats scissors";
     }
     if (playerSelection == "paper" && computerSelection == "scissors")
     {
         pcScore += 1;
-        return "You lose, scissors cut paper";
+        roundDetails.textContent =  "You lose, scissors cut paper";
     }
     if (playerSelection == "paper" && computerSelection == "rock")
     {
         playerScore += 1;
-        return "You win, paper beats rock"; 
+        roundDetails.textContent =  "You win, paper beats rock"; 
     }
     if (playerSelection == "scissors" && computerSelection == "rock")
     {
         pcScore += 1;
-        return "You lose, rock beats scissors"; 
+        roundDetails.textContent =  "You lose, rock beats scissors"; 
     }
     if (playerSelection == "scissors" && computerSelection == "paper")
     {
         playerScore += 1;
-        return "You win, scissors cut paper"; 
+        roundDetails.textContent =  "You win, scissors cut paper"; 
     }
+    
 }
 
 function playerPlay(){
     let clickedButton = "";
-
-    const buttons = Array.from(document.querySelectorAll('button'));
-    console.log(buttons);
+  
+    //console.log(buttons);
     
     buttons.forEach((button) => {
     button.addEventListener('click', () => {
         clickedButton = button.id.toString();
-        console.log(playRound(clickedButton));
+        playRound(clickedButton);
       
     });
     });
 }
-function game()
-{
-    
-    
+function game() {
+
         playerPlay();
-        if (pcScore > playerScore)
-            console.log("PC WINS");
-        else if (pcScore < playerScore)
-            console.log("PLAYER WINS");
-        else
-            console.log("It's not possible, but it's a tie");
-    
-    
+       
+}
 
-}//ask to play again
+function gameOver() {
+    buttons.forEach(button => {
+        button.disabled = true;
+    });
+    resultsBox.appendChild(restart);
+    restart.textContent = "Play again?";
+    restart.addEventListener('click', () => {
+        window.location.reload();
+    });
+}
 
+function updateScore (){
+    
+    score.textContent = "PC: "+ pcScore + " Player: " + playerScore; 
+    
+    if ((pcScore == winScore)||(playerScore == winScore)) {
+        if (pcScore == winScore) 
+            winner.textContent = "PC WINS";
+        else if (playerScore == winScore) 
+            winner.textContent = "PLAYER WINS";
+       gameOver();
+    }
+
+}
+
+    
 
 
 
